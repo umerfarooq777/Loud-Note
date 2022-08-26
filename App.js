9ed03990810af87ed0c88ee3a48ca3e3474f1389
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-08-26 16:30:19 +0500

    new

--- App.js
+++ App.js
@@ -1,123 +1,33 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-// import Header1 from './components/header'
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Header1 from './components/header'
 
-// const App= () => {
+const App= () => {
  
-//   return (
-//     <View>
-//       <Text>Hello</Text>
-//     </View>
-   
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   container: {
-//     width:'95%',
-//     marginRight: 'auto',
-//     marginLeft: 'auto',
-//     backgroundColor:'#6ED4C8',
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
-import React, {useState} from 'react';
-import {SafeAreaView, View, StyleSheet, FlatList, Text} from 'react-native';
-import ItemBox from './src/components/ItemBox';
-
-const data = [
-  {id: '1', name: 'A'},
-  {id: '2', name: 'B'},
-  {id: '3', name: 'C'},
-  {id: '4', name: 'D'},
-  {id: '5', name: 'E'},
-  {id: '6', name: 'F'},
-  {id: '7', name: 'G'},
-  {id: '8', name: 'H'},
-  {id: '9', name: 'I'},
-  {id: '10', name: 'J'},
-  {id: '11', name: 'K'},
-  {id: '12', name: 'L'},
-  {id: '13', name: 'M'},
-  {id: '14', name: 'N'},
-  {id: '15', name: 'O'},
-  {id: '16', name: 'P'},
-  {id: '17', name: 'Q'},
-  {id: '18', name: 'R'},
-  {id: '19', name: 'S'},
-  {id: '20', name: 'T'},
-  {id: '21', name: 'U'},
-  {id: '22', name: 'V'},
-  {id: '23', name: 'W'},
-  {id: '24', name: 'X'},
-  {id: '25', name: 'Y'},
-  {id: '26', name: 'Z'},
-];
-
-const App = () => {
-  const [lists, setLists] = useState(data);
-
-  const deleteItem = (index) => {
-    const arr = [...lists];
-    arr.splice(index, 1);
-    setLists(arr);
-  };
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={lists}
-        renderItem={({item, index}) => {
-          return <ItemBox data={item} handleDelete={() => deleteItem(index)} />;
-        }}
-        ItemSeparatorComponent={() => {
-          return <View style={styles.seperatorLine}></View>;
-        }}
-      />
-    </SafeAreaView>
+    <View>
+      <Header1/>
+    </View>
+   
   );
 };
 
-export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  seperatorLine: {
-    height: 1,
-    backgroundColor: 'black',
-  },
-});
+
+export default App;
--- components/header.js
+++ components/header.js
@@ -1,55 +1,55 @@
-// import React from 'react'
-// import {Text, View} from 'react-native'
-// const Header = () => {
-//   return (
-//     <View style={{textAlign:'center',borderRadius:10,bac}}>
+import React from 'react'
+import {Text, View} from 'react-native'
+const Header = () => {
+  return (
+    <View style={{textAlign:'center',borderRadius:10}}>
 
-//     <Text style={{fontSize:30 ,marginTop:30,marginBottom:30,textAlign:'center',fontWeight:'bold'}}>Today's Tasks</Text>
-//     </View>
-//   )
-// }
+    <Text style={{fontSize:30 ,marginTop:30,marginBottom:30,textAlign:'center',fontWeight:'bold'}}>Today's Tasks</Text>
+    </View>
+  )
+}
 
 // export default Header
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  Animated,
-  TouchableOpacity,
-} from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
+// import React from 'react';
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   Dimensions,
+//   Animated,
+//   TouchableOpacity,
+// } from 'react-native';
+// import Swipeable from 'react-native-gesture-handler/Swipeable';
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+// const SCREEN_WIDTH = Dimensions.get('window').width;
 
-const ItemBox = (props) => {
-  const leftSwipe = (progress, dragX) => {
-    const scale = dragX.interpolate({
-      inputRange: [0, 100],
-      outputRange: [0, 1],
-      extrapolate: 'clamp',
-    });
-    return (
-      <TouchableOpacity onPress={props.handleDelete} activeOpacity={0.6}>
-        <View style={styles.deleteBox}>
-          <Animated.Text style={{transform: [{scale: scale}]}}>
-            Delete
-          </Animated.Text>
-        </View>
-      </TouchableOpacity>
-    );
-  };
-  return (
-    <Swipeable renderLeftActions={leftSwipe}>
-      <View style={styles.container}>
-        <Text>My name is {props.data.name}.</Text>
-      </View>
-    </Swipeable>
-  );
-};
+// const ItemBox = (props) => {
+//   const leftSwipe = (progress, dragX) => {
+//     const scale = dragX.interpolate({
+//       inputRange: [0, 100],
+//       outputRange: [0, 1],
+//       extrapolate: 'clamp',
+//     });
+//     return (
+//       <TouchableOpacity onPress={props.handleDelete} activeOpacity={0.6}>
+//         <View style={styles.deleteBox}>
+//           <Animated.Text style={{transform: [{scale: scale}]}}>
+//             Delete
+//           </Animated.Text>
+//         </View>
+//       </TouchableOpacity>
+//     );
+//   };
+//   return (
+//     <Swipeable renderLeftActions={leftSwipe}>
+//       <View style={styles.container}>
+//         <Text>My name is {props.data.name}.</Text>
+//       </View>
+//     </Swipeable>
+//   );
+// };
 
-export default ItemBox;
+// export default ItemBox;
 
 const styles = StyleSheet.create({
   container: {
